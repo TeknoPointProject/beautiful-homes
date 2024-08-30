417ac83d83a79058409cd6eec7ac70d422ec36e1
Kaushik Khara <Kaushik.Khara@teknopoint.local>
2024-08-30 16:03:41 +0530

    unminify scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,171 @@
-import{sampleRUM,loadHeader,loadFooter,decorateButtons,decorateIcons,decorateSections,decorateBlocks,decorateTemplateAndTheme,waitForLCP,loadBlocks,loadCSS}from"./aem.js";const LCP_BLOCKS=[];export function moveAttributes(e,t,o){o||(o=[...e.attributes].map((({nodeName:e})=>e))),o.forEach((o=>{const a=e.getAttribute(o);a&&(t.setAttribute(o,a),e.removeAttribute(o))}))}export function moveInstrumentation(e,t){moveAttributes(e,t,[...e.attributes].map((({nodeName:e})=>e)).filter((e=>e.startsWith("data-aue-")||e.startsWith("data-richtext-"))))}async function loadFonts(){await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);try{window.location.hostname.includes("localhost")||sessionStorage.setItem("fonts-loaded","true")}catch(e){}}function buildAutoBlocks(){}export function decorateMain(e){decorateButtons(e),decorateIcons(e),buildAutoBlocks(e),decorateSections(e),decorateBlocks(e)}async function loadEager(e){document.documentElement.lang="en",decorateTemplateAndTheme();const t=e.querySelector("main");t&&(decorateMain(t),document.body.classList.add("appear"),await waitForLCP(LCP_BLOCKS));try{(window.innerWidth>=900||sessionStorage.getItem("fonts-loaded"))&&loadFonts()}catch(e){}}async function loadLazy(e){const t=e.querySelector("main");await loadBlocks(t);const{hash:o}=window.location,a=!!o&&e.getElementById(o.substring(1));o&&a&&a.scrollIntoView(),loadHeader(e.querySelector("header")),loadFooter(e.querySelector("footer")),loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`),loadFonts(),sampleRUM("lazy"),sampleRUM.observe(t.querySelectorAll("div[data-block-name]")),sampleRUM.observe(t.querySelectorAll("picture > img"))}function loadDelayed(){window.setTimeout((()=>import("./delayed.js")),3e3),import("./sidekick.js").then((({initSidekick:e})=>e()))}async function loadingCustomCss(){[`${window.hlx.codeBasePath}/styles/legacy-container/legacy-container.css`,`${window.hlx.codeBasePath}/styles/design-gallery/design-gallery.css`,`${window.hlx.codeBasePath}/styles/simple-outer/simple-outer.css`,`${window.hlx.codeBasePath}/styles/home-improve/home-improve.css`,`${window.hlx.codeBasePath}/styles/head-redesign/head-redesign.css`,`${window.hlx.codeBasePath}/styles/testimonial-wrapper/testimonial-wrapper.css`].forEach((async e=>{await loadCSS(e)}))}async function loadPage(){await loadEager(document),await loadLazy(document),await loadingCustomCss(),loadDelayed()}loadPage();
+import {
+  sampleRUM,
+  loadHeader,
+  loadFooter,
+  decorateButtons,
+  decorateIcons,
+  decorateSections,
+  decorateBlocks,
+  decorateTemplateAndTheme,
+  waitForLCP,
+  loadBlocks,
+  loadCSS,
+} from './aem.js';
+
+const LCP_BLOCKS = []; // add your LCP blocks to the list
+
+/**
+ * Moves all the attributes from a given elmenet to another given element.
+ * @param {Element} from the element to copy attributes from
+ * @param {Element} to the element to copy attributes to
+ */
+export function moveAttributes(from, to, attributes) {
+  if (!attributes) {
+    // eslint-disable-next-line no-param-reassign
+    attributes = [...from.attributes].map(({ nodeName }) => nodeName);
+  }
+  attributes.forEach((attr) => {
+    const value = from.getAttribute(attr);
+    if (value) {
+      to.setAttribute(attr, value);
+      from.removeAttribute(attr);
+    }
+  });
+}
+
+/**
+ * Move instrumentation attributes from a given element to another given element.
+ * @param {Element} from the element to copy attributes from
+ * @param {Element} to the element to copy attributes to
+ */
+export function moveInstrumentation(from, to) {
+  moveAttributes(
+    from,
+    to,
+    [...from.attributes]
+      .map(({ nodeName }) => nodeName)
+      .filter((attr) => attr.startsWith('data-aue-') || attr.startsWith('data-richtext-')),
+  );
+}
+
+/**
+ * load fonts.css and set a session storage flag
+ */
+async function loadFonts() {
+  await loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`);
+  try {
+    if (!window.location.hostname.includes('localhost')) sessionStorage.setItem('fonts-loaded', 'true');
+  } catch (e) {
+    // do nothing
+  }
+}
+
+/**
+ * Builds all synthetic blocks in a container element.
+ * @param {Element} main The container element
+ */
+function buildAutoBlocks() {
+  try {
+    // TODO: add auto block, if needed
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Auto Blocking failed', error);
+  }
+}
+
+/**
+ * Decorates the main element.
+ * @param {Element} main The main element
+ */
+// eslint-disable-next-line import/prefer-default-export
+export function decorateMain(main) {
+  // hopefully forward compatible button decoration
+  decorateButtons(main);
+  decorateIcons(main);
+  buildAutoBlocks(main);
+  decorateSections(main);
+  decorateBlocks(main);
+}
+
+/**
+ * Loads everything needed to get to LCP.
+ * @param {Element} doc The container element
+ */
+async function loadEager(doc) {
+  document.documentElement.lang = 'en';
+  decorateTemplateAndTheme();
+  const main = doc.querySelector('main');
+  if (main) {
+    decorateMain(main);
+    document.body.classList.add('appear');
+    await waitForLCP(LCP_BLOCKS);
+  }
+
+  try {
+    /* if desktop (proxy for fast connection) or fonts already loaded, load fonts.css */
+    if (window.innerWidth >= 900 || sessionStorage.getItem('fonts-loaded')) {
+      loadFonts();
+    }
+  } catch (e) {
+    // do nothing
+  }
+}
+
+/**
+ * Loads everything that doesn't need to be delayed.
+ * @param {Element} doc The container element
+ */
+async function loadLazy(doc) {
+  const main = doc.querySelector('main');
+  await loadBlocks(main);
+
+  const { hash } = window.location;
+  const element = hash ? doc.getElementById(hash.substring(1)) : false;
+  if (hash && element) element.scrollIntoView();
+
+  loadHeader(doc.querySelector('header'));
+  loadFooter(doc.querySelector('footer'));
+
+  loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
+  loadFonts();
+
+  sampleRUM('lazy');
+  sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
+  sampleRUM.observe(main.querySelectorAll('picture > img'));
+}
+
+/**
+ * Loads everything that happens a lot later,
+ * without impacting the user experience.
+ */
+function loadDelayed() {
+  // eslint-disable-next-line import/no-cycle
+  window.setTimeout(() => import('./delayed.js'), 3000);
+  // load anything that can be postponed to the latest here
+  import('./sidekick.js').then(({ initSidekick }) => initSidekick());
+}
+
+async function loadingCustomCss() {
+  // load custom css files
+  var loadCssArray = [
+    `${window.hlx.codeBasePath}/styles/legacy-container/legacy-container.css`,
+    `${window.hlx.codeBasePath}/styles/design-gallery/design-gallery.css`,
+    `${window.hlx.codeBasePath}/styles/simple-outer/simple-outer.css`,
+    `${window.hlx.codeBasePath}/styles/home-improve/home-improve.css`,
+    `${window.hlx.codeBasePath}/styles/head-redesign/head-redesign.css`,
+    `${window.hlx.codeBasePath}/styles/testimonial-wrapper/testimonial-wrapper.css`
+
+  ];
+  loadCssArray.forEach(async (eachCss) => {
+    await loadCSS(eachCss);
+  });
+}
+
+async function loadPage() {
+  await loadEager(document);
+  await loadLazy(document);
+  await loadingCustomCss();
+  loadDelayed();
+}
+
+loadPage();
