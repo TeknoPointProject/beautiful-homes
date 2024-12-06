7bb4c1bd53750b870d3fff2f3c99ce2eadb619c5
TeknoPointProject <146836174+TeknoPointProject@users.noreply.github.com>
2024-12-06 12:29:23 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,27 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './aem.js';
+import { fetchPlaceholders, sampleRUM } from './aem.js';
+
+
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
+
+
+
 // add more delayed functionality here
+function loadHeadAnalytics() {
+  const scriptTag = document.createElement('script');
+
+  scriptTag.innerHTML = `https://assets.adobedtm.com/ef0f7eb243a4/86be44fc9fd9/launch-03b4278cb656-development.min.js`;
+
+  document.head.prepend(scriptTag);
+}
+
+
+
+
+
+if (!window.location.hostname.includes('localhost') && !window.location.hostname.includes('author')) {
+    loadHeadAnalytics();
+}
