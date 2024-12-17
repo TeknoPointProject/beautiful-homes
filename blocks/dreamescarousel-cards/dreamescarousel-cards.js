ae27f16a0db47f31c425c551d632fab9da4f9c43
TeknoPointProject <146836174+TeknoPointProject@users.noreply.github.com>
2024-12-17 21:00:42 +0530

    Update dreamescarousel-cards.js

--- blocks/dreamescarousel-cards/dreamescarousel-cards.js
+++ blocks/dreamescarousel-cards/dreamescarousel-cards.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 
     $videocarousel.slick({
         infinite: true,
-        slidesToShow: 1,
+        slidesToShow: 3,
         slidesToScroll: 1,
         dots: true,
         arrows: true,
