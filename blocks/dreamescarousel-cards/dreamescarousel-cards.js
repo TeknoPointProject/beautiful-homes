c1be0c3ffcb4b05a9f1f5c73c65ad52ee20f3330
TeknoPointProject <146836174+TeknoPointProject@users.noreply.github.com>
2024-12-18 00:47:03 +0530

    Update dreamescarousel-cards.js

--- blocks/dreamescarousel-cards/dreamescarousel-cards.js
+++ blocks/dreamescarousel-cards/dreamescarousel-cards.js
@@ -4,116 +4,114 @@ $(document).ready(function () {
     var $designgallery = $('.design-gallery .carousel-wrapper');
     var $fullhome = $('.fullhome .carousel-wrapper');
     var $centerslide = $('.center-slider .carousel-wrapper');
-    
-    // Initialize Slick carousel
-    $carouselContainer.slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      dots: true,
-      arrows: true,
-      autoplay: true,
-      autoplaySpeed: 2000
-    });
 
-
-    $videocarousel.slick({
-        infinite: false,
-        arrows: true,
-        slidesToShow: 3.14,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-              arrows: false,
-              dots: true,
-              slidesToShow: 1.03,
-            },
-          },
-        ],
-        
-      });
-
-
-      $designgallery.slick({
-        infinite: false,
-      arrows: true,
-      slidesToShow: 3.14,
-      slidesToScroll: 1,
-      responsive: [
-        {
-          breakpoint: 992,
-          settings: {
-            arrows: false,
+    if ($carouselContainer.length) {
+        $carouselContainer.slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
             dots: true,
-            slidesToShow: 1.03,
-          },
-        },
-      ],
-        
-      });
-     
-      $fullhome.slick({
-        infinite: false,
-        arrows: true,
-        slidesToShow: 3.14,
-        slidesToScroll: 1,
-        responsive: [
-          {
-            breakpoint: 992,
-            settings: {
-              slidesToShow: 1.03,
-              slidesToScroll: 1,
-              infinite: false,
-              dots: true,
-              arrows: false,
-            },
-          },
-        ],
-        
-      });
+            arrows: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+        });
+    }
 
-      $centerslide.slick({
-        centerMode: true,
-        centerPadding: "280px",
-        slidesToShow: 1,
-        focusOnSelect: true,
-        infinite: true,
-        autoplay: true,
-        cssEase: "linear",
-        speed: 1800,
-        arrows: true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              centerMode: true,
-              centerPadding: "60px",
-              slidesToShow: 1,
-            },
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              centerMode: true,
-              centerPadding: "40px",
-              slidesToShow: 1,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              centerMode: true,
-              centerPadding: "30px",
-              slidesToShow: 1,
-            },
-          },
-        ],
-        
-      });
+    if ($videocarousel.length) {
+        $videocarousel.slick({
+            infinite: false,
+            arrows: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        arrows: false,
+                        dots: true,
+                        slidesToShow: 1,
+                    },
+                },
+            ],
+        });
+    }
 
+    if ($designgallery.length) {
+        $designgallery.slick({
+            infinite: false,
+            arrows: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        arrows: false,
+                        dots: true,
+                        slidesToShow: 1,
+                    },
+                },
+            ],
+        });
+    }
 
+    if ($fullhome.length) {
+        $fullhome.slick({
+            infinite: false,
+            arrows: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        dots: true,
+                        arrows: false,
+                    },
+                },
+            ],
+        });
+    }
 
-
-  });
+    if ($centerslide.length) {
+        $centerslide.slick({
+            centerMode: true,
+            centerPadding: "60px", // Adjust padding
+            slidesToShow: 1,
+            focusOnSelect: true,
+            infinite: true,
+            autoplay: true,
+            cssEase: "linear",
+            speed: 1800,
+            arrows: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        centerMode: true,
+                        centerPadding: "40px",
+                        slidesToShow: 1,
+                    },
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        centerMode: true,
+                        centerPadding: "30px",
+                        slidesToShow: 1,
+                    },
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        centerMode: true,
+                        centerPadding: "20px",
+                        slidesToShow: 1,
+                    },
+                },
+            ],
+        });
+    }
+});
